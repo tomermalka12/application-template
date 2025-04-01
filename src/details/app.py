5edab6935443227cc415c59d2c488ebddc067886
tomer malka pinto <tomermalka12@gmail.com>
2025-04-01 18:00:42 +0300

    3 commit

--- src/details/app.py
+++ src/details/app.py
@@ -1,4 +1,21 @@
-from flask import FLASK
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+@app.route('/<name>')
+def hello(name):
+    return f'Alex says: hello {name}'
+
+@app.route('/check_file/<file_name>')
+def check_file(file_name):
+    try:
+        with open(file_name) as FILE:
+            data=FILE.readline()
+        return data
+    except FileNotFoundError:
+        return f'[!] no such file {file_name} Found'
